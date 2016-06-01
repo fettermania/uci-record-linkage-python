543a34dfd0a4e6bf5e34630f0daadef09922bd69
Dave Fetterman <fettermania@gmail.com>
2016-06-01 10:13:14 -0700

    Graph components library v0.1

--- graph_components.py
+++ graph_components.py
@@ -1,76 +1,124 @@
+import pandas as pd
+import numpy as np
+
 # graph_components.py
+
+# Basic idea: component ids are the same space as node ids,
+# and are the same id as the lowest node_id in that component.
 class GraphComponents(object):
 
   def __init__(self, df_ids):
-    self.id_to_component_map = {} # int -> int
+    self.edges = {}
+    self.nodes = []
+
+    self.node_to_component_map = {} # int -> int
     self.component_to_ids_map = {} # int -> set
-    self.next_component_id = 0;
+
     df_ids_array = df_ids.values
-    
     # Fettermania; Could possibly remove this, doing it inline
-    for ids in df_ids_array:
-      self._add_id_to_component(ids[0], ids[0])
-      self._add_id_to_component(ids[1], ids[1])
+    for edge in df_ids_array:
+      # print("ADDING NODE %d" % edge[0])
+      self.add_node(edge[0])
+      # print("ADDING NODE %d" % edge[1])
+      self.add_node(edge[1])
 
-    for ids in df_ids_array:
-      self._add_edge(ids[0], ids[1])
+    # print("ID -> COMPONENT")
+    # print(self.node_to_component_map);
 
-    # Fettermania TODO: possibly could walk backwards instead of doing ALL
-    for ids in df_ids_array:
-      self._resolve_component_for_id(ids[0])
-      self._resolve_component_for_id(ids[1])
+    # print("COMPONENT -> IDS")
+    # print(self.component_to_ids_map);
 
-  def get_ids_in_component(component):
+    for edge in df_ids_array:
+      self.add_edge(edge[0], edge[1])
+
+  def get_components(self):
+    return self.component_to_ids_map
+
+  def get_nodes_in_component(self, component):
     return self.component_to_ids_map(component)
 
-  def get_component_for_id(id):
-    return self.id_to_component_map[id]
+  def get_component_for_node(self, node_id):
+    return self.node_to_component_map[node]
 
-  def _add_id_to_component(id, component):
-    # remove id from set in component -> {ids_set}
-    if id in self.id_to_component_map:
-      old_component = self.id_to_component[id]
-      self._remove_id_from_component(old_component)
+  def add_node(self, node_id):
+    if node_id not in self.node_to_component_map:
+      self._add_node_to_component(node_id, node_id)
+      self.nodes.append(node_id)
 
-    # (re)assign id -> component
-    id_to_component_map[id] = component
-    
-    # add: if component exists, add.  Else create singleton set.
-    if component in component_to_ids_map:
-      component_to_ids[component].add(id)
+  def add_edge(self, node_id_1, node_id_2):
+    if node_id_1 in self.edges:
+      self.edges[node_id_1].add(node_id_2)
     else:
-      component_to_ids[component] = set([id])
-  
-  def _remove_id_from_component(id, component):
-    if component in component_to_ids_map:
-      self.component_to_ids_map[component].remove(id)
+      self.edges[node_id_1] = set([node_id_2])
 
-  def _add_edge(id1, id2):
-    component_1 = self.id_to_component_map[id1]
-    component_2 = self.id_to_component_map[id2]
+
+    if node_id_2 in self.edges:
+      self.edges[node_id_2].add(node_id_1)
+    else:
+      self.edges[node_id_2] = set([node_id_1])
+    
+
+    # print("ADDING EDGE %d -- %d" % (node_id_1, node_id_2))
+    component_1 = self.node_to_component_map[node_id_1]
+    component_2 = self.node_to_component_map[node_id_2]
     min_component = min(component_1, component_2)
 
+    # print (" .... MIN COMPONENT IS %d" % min_component)
+
     if component_1 != min_component:
-      self._remove_id_from_component(id_1, component_1)
+
+      self._add_node_to_component(node_id_1, min_component)
+      self._remap_component_to_new_id(component_1, min_component)
 
     if component_2 != min_component:
-      self._remove_id_from_component(id_2, component_2)
+      self._add_node_to_component(node_id_2, min_component)
+      self._remap_component_to_new_id(component_2, min_component)
 
-    self._add_id_to_component(id1, min_component)
-    self._add_id_to_component(id2, min_component)
+  # Invariant: Node_id is in exactly one component
+  def _add_node_to_component(self, node_id, component):
+    # remove node_id from set in component -> {node_ids_set}
+    if node_id in self.node_to_component_map:
+      old_component = self.node_to_component_map[node_id]
+      if old_component != component:
+        self._helper_remove_id_from_component(node_id, old_component)
 
-  def _resolve_component_for_id(id):
-    to_resolve = []
-    last_component_id = id
-    next_component_id = self.id_to_component_map[last_component_id]
+    # (re)assign node_id -> component
+    self.node_to_component_map[node_id] = component
+    
+    # add node_id to component
+    # add: if component's node_id set exists, add.  Else create singleton set.
+    if component in self.component_to_ids_map:
+      self.component_to_ids_map[component].add(node_id)
+    else:
+      self.component_to_ids_map[component] = set([node_id])
+  
+  # Helper: Only called by _add_node_to_component
+  # Fettermania: Basically "remove node from graph"
+  def _helper_remove_id_from_component(self, node_id, component_id):
+    if component_id in self.component_to_ids_map:
+      self.component_to_ids_map[component_id].remove(node_id)
+      if len(self.component_to_ids_map[component_id]) == 0:
+        # print("Remove: COMPONENT %d is " % component_id)
+        # print(self.component_to_ids_map[component_id])
+        # print("Remove: DESTROYING COMPONENT %d" % component_id)
+        del self.component_to_ids_map[component_id]
 
-    while (next_component_id != last_component_id):
-      to_resolve.append(last_component_id)
-      last_component_id = next_component_id
-      next_component_id = self.id_to_component_map[next_component_id]
+    if node_id in self.node_to_component_map:
+      del self.node_to_component_map[node_id]
 
-    for id_to_resolve in to_resolve:
-      self._add_id_to_component(id_to_resolve, last_component_id)
+  def _remap_component_to_new_id(self, component_id, new_component_id):
+    # print ("REMAPPING COMPONENT %d to %d" % (component_id, new_component_id))
+    if component_id in self.component_to_ids_map: # Fettermania TODO gross
+      node_ids = self.component_to_ids_map[component_id].copy()
 
+      for node_id in node_ids:
+        self._add_node_to_component(node_id, new_component_id)
 
+def get_test_data_frame():
+  # s1 = pd.Series([1,1,2,3,5])
+  # s2 = pd.Series([2,3,4,6,5])
+  s1 = pd.Series( [np.random.random_integers(1,30) for i in range(10)])
+  s2 = pd.Series( [np.random.random_integers(1,30) for i in range(10)])
+  df = pd.DataFrame({'id_1': s1, 'id_2': s2})
+  return df
 
